fea77bc535b41ce4511db62d8937bcb3a71524d1
Aditya Agarwal <adityatd189@gmail.com>
2022-06-11 22:53:02 +0530

    Update celsius_to_farehnite.py

--- celsius_to_farehnite.py
+++ celsius_to_farehnite.py
@@ -1,7 +1,5 @@
 celsius_1 = float(input("Temperature value in degree Celsius: " ))  
   
-# For Converting the temperature to degree Fahrenheit by using the above  
-# given formula  
 Fahrenheit_1 = (celsius_1 * 1.8) + 32  
     
 # print the result  
